9ad9d85f0398bd2aa3d5e41a254ab7da503c8757
SumitChatterjeee <subrothochatt123@gmail.com>
2025-12-01 19:03:19 +0530

    Variables are added

---
+++ src/main/java/com/lovable/ai/Entities/Project.java
@@ -0,0 +1,10 @@
+package com.lovable.ai.Entities;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Project {
+    Long id;
+    String name;
+}
